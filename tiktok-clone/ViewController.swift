a3a41e79b5747ba4cb5a6934cc762776c34eb386
Márcio Vinícius <marcio.campos@outlook.com>
2020-07-20 12:10:44 -0300

    first commit

---
+++ tiktok-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tiktok-clone
+//
+//  Created by Marcio Vinicius Campos da Silveira on 20/07/20.
+//  Copyright © 2020 MarcioVCampos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
