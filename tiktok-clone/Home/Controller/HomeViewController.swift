ae6f648acee03786dae3f4d2e19bdd3971e6dc9f
Márcio Vinícius <marcio.campos@outlook.com>
2020-07-23 12:50:12 -0300

    change status bar color for Home

--- tiktok-clone/Home/Controller/HomeViewController.swift
+++ tiktok-clone/Home/Controller/HomeViewController.swift
@@ -43,6 +43,37 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
         let cvRect = collectionView.frame
         return CGSize(width: cvRect.width, height: cvRect.height)
     }
+    
+    // Change Status Bar Color
+    
+    override func viewDidAppear(_ animated: Bool) {
+     if #available(iOS 13.0, *) {
+         let app = UIApplication.shared
+         let statusBarHeight: CGFloat = app.statusBarFrame.size.height
+         
+         let statusbarView = UIView()
+         statusbarView.backgroundColor = UIColor.black
+         view.addSubview(statusbarView)
+       
+         statusbarView.translatesAutoresizingMaskIntoConstraints = false
+         statusbarView.heightAnchor
+             .constraint(equalToConstant: statusBarHeight).isActive = true
+         statusbarView.widthAnchor
+             .constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
+         statusbarView.topAnchor
+             .constraint(equalTo: view.topAnchor).isActive = true
+         statusbarView.centerXAnchor
+             .constraint(equalTo: view.centerXAnchor).isActive = true
+       
+     } else {
+         let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView
+         statusBar?.backgroundColor = UIColor.red
+     }
+    }
+      
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
 
 }
 
