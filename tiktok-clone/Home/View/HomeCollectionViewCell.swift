95bc66ed80b050ab459051f8d8be72afc36a71e5
Márcio Vinícius <marcio.campos@outlook.com>
2020-07-23 12:54:58 -0300

    remove unused code

--- tiktok-clone/Home/View/HomeCollectionViewCell.swift
+++ tiktok-clone/Home/View/HomeCollectionViewCell.swift
@@ -9,13 +9,6 @@
 import UIKit
 import AVFoundation
 
-protocol HomeCollectionViewCellDelegate: AnyObject {
-    func didTapLikeButton(with video: Video)
-    func didTapProfileButton(with video: Video)
-    func didTapShareButton(with video: Video)
-    func didTapCommentButton(with video: Video)
-}
-
 class HomeCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var videoContainer: UIView!
@@ -27,66 +20,12 @@ class HomeCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "cellVideo"
     
-    // MARK: Labels
-    
-    private let usernameLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .left
-        label.textColor = .white
-        return label
-    }()
-    
-    private let captionLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .left
-        label.textColor = .white
-        return label
-    }()
-    
-    private let audioLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .left
-        label.textColor = .white
-        return label
-    }()
-    
-    // MARK: Buttons
-    
-    private let profileButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(systemName: "person.circle"), for: .normal)
-        return button
-    }()
-    
-    private let likeButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(systemName: "heart.fill"), for: .normal)
-        return button
-    }()
-    
-    private let commentButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(systemName: "text.bubble.fill"), for: .normal)
-        return button
-    }()
-    
-    private let shareButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(systemName: "phone.circle.fill"), for: .normal)
-        return button
-    }()
-    
-    // MARK: Delegate
-    
-    weak var delegate: HomeCollectionViewCell?
-    
     // MARK: Subviews
     private var video: Video?
     var player: AVPlayer?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubviews()
     }
     
     public func configure(with video: Video){
@@ -95,65 +34,11 @@ class HomeCollectionViewCell: UICollectionViewCell {
         configureVideoInfo()
     }
     
-    private func addSubviews(){
-        contentView.addSubview(usernameLabel)
-        contentView.addSubview(captionLabel)
-        contentView.addSubview(audioLabel)
-        
-        
-        contentView.addSubview(profileButton)
-        contentView.addSubview(likeButton)
-        contentView.addSubview(commentButton)
-        contentView.addSubview(shareButton)
-        
-        // Add actions
-        profileButton.addTarget(self, action: #selector(didTapProfileButton), for: .touchDown)
-        likeButton.addTarget(self, action: #selector(didTapLikeButton), for: .touchDown)
-        commentButton.addTarget(self, action: #selector(didTapCommentButton), for: .touchDown)
-        shareButton.addTarget(self, action: #selector(didTapShareButton), for: .touchDown)
-       
-    }
-    
-    @objc private func didTapProfileButton(with: Video){
-           guard let video = video else { return }
-           delegate?.didTapProfileButton(with: video)
-       }
-    
-    @objc private func didTapLikeButton(with: Video){
-        guard let video = video else { return }
-        delegate?.didTapLikeButton(with: video)
-    }
-    
-    @objc private func didTapCommentButton(with: Video){
-          guard let video = video else { return }
-          delegate?.didTapCommentButton(with: video)
-    }
-    
-    @objc private func didTapShareButton(with: Video){
-          guard let video = video else { return }
-          delegate?.didTapShareButton(with: video)
-    }
-    
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        let size = contentView.frame.size.width/6
-        let width = contentView.frame.size.width
-        let heigh = contentView.frame.size.height
-        
-        //Buttons
-        profileButton.frame = CGRect(x: width-size, y: heigh-size, width: size, height: size)
-        likeButton.frame = CGRect(x: width-size, y: heigh-(size*2)-10, width: size, height: size)
-        commentButton.frame = CGRect(x: width-size, y: heigh-(size*3)-10, width: size, height: size)
-        shareButton.frame = CGRect(x: width-size, y: heigh-(size*4)-10, width: size, height: size)
-    }
-    
     override func prepareForReuse() {
         super.prepareForReuse()
-        captionLabel.text = nil
-        audioLabel.text = nil
-        usernameLabel.text = nil
+        labelUsername.text = nil
+        labelCaption.text = nil
+        labelAudioTrack.text = nil
     }
     
     private func configureVideo(){
